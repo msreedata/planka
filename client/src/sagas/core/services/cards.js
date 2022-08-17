48cb046797ce050f52f7d32dde9339a8d3bd973b
Sreejith <Sreejith.Mani@gmail.com>
2022-08-17 11:11:37 +0200

    ..

--- client/src/sagas/core/services/cards.js
+++ client/src/sagas/core/services/cards.js
@@ -7,147 +7,153 @@ import actions from '../../../actions';
 import api from '../../../api';
 import { createLocalId } from '../../../utils/local-id';
 
+export function* createSingleCard(listId, data) {
+        const { boardId } = yield select(selectors.selectListById, listId);
+        const nextData = {
+            ...element,
+            listId,
+            position: yield select(selectors.selectNextCardPosition, listId),
+        };
+
+        const localId = yield call(createLocalId);
+
+        yield put(
+            actions.createCard({
+                ...nextData,
+                boardId,
+                id: localId,
+            }),
+        );
+
+        let card;
+        try {
+            ({ item: card } = yield call(request, api.createCard, boardId, nextData));
+        } catch (error) {
+            yield put(actions.createCard.failure(localId, error));
+            return;
+        }
+
+        yield put(actions.createCard.success(localId, card));
+}
+
 export function* createCard(listId, data) {
-  const { boardId } = yield select(selectors.selectListById, listId);
-
-  const nextData = {
-    ...data,
-    listId,
-    position: yield select(selectors.selectNextCardPosition, listId),
-  };
-
-  const localId = yield call(createLocalId);
-
-  yield put(
-    actions.createCard({
-      ...nextData,
-      boardId,
-      id: localId,
-    }),
-  );
-
-  let card;
-  try {
-    ({ item: card } = yield call(request, api.createCard, boardId, nextData));
-  } catch (error) {
-    yield put(actions.createCard.failure(localId, error));
-    return;
-  }
-
-  yield put(actions.createCard.success(localId, card));
+    (data.toString().split('\n')).forEach(element => {
+      createSingleCard(listId, data)
+    });
 }
 
 export function* handleCardCreate(card) {
-  yield put(actions.handleCardCreate(card));
+    yield put(actions.handleCardCreate(card));
 }
 
 export function* updateCard(id, data) {
-  yield put(actions.updateCard(id, data));
+    yield put(actions.updateCard(id, data));
 
-  let card;
-  try {
-    ({ item: card } = yield call(request, api.updateCard, id, data));
-  } catch (error) {
-    yield put(actions.updateCard.failure(id, error));
-    return;
-  }
+    let card;
+    try {
+        ({ item: card } = yield call(request, api.updateCard, id, data));
+    } catch (error) {
+        yield put(actions.updateCard.failure(id, error));
+        return;
+    }
 
-  yield put(actions.updateCard.success(card));
+    yield put(actions.updateCard.success(card));
 }
 
 export function* updateCurrentCard(data) {
-  const { cardId } = yield select(selectors.selectPath);
+    const { cardId } = yield select(selectors.selectPath);
 
-  yield call(updateCard, cardId, data);
+    yield call(updateCard, cardId, data);
 }
 
 export function* handleCardUpdate(card) {
-  yield put(actions.handleCardUpdate(card)); // TODO: handle card transfer
+    yield put(actions.handleCardUpdate(card)); // TODO: handle card transfer
 }
 
 export function* moveCard(id, listId, index) {
-  const position = yield select(selectors.selectNextCardPosition, listId, index, id);
+    const position = yield select(selectors.selectNextCardPosition, listId, index, id);
 
-  yield call(updateCard, id, {
-    listId,
-    position,
-  });
+    yield call(updateCard, id, {
+        listId,
+        position,
+    });
 }
 
 export function* moveCurrentCard(listId, index) {
-  const { cardId } = yield select(selectors.selectPath);
+    const { cardId } = yield select(selectors.selectPath);
 
-  yield call(moveCard, cardId, listId, index);
+    yield call(moveCard, cardId, listId, index);
 }
 
 export function* transferCard(id, boardId, listId, index) {
-  const { cardId: currentCardId, boardId: currentBoardId } = yield select(selectors.selectPath);
-  const position = yield select(selectors.selectNextCardPosition, listId, index, id);
+    const { cardId: currentCardId, boardId: currentBoardId } = yield select(selectors.selectPath);
+    const position = yield select(selectors.selectNextCardPosition, listId, index, id);
 
-  if (id === currentCardId) {
-    yield call(goToBoard, currentBoardId);
-  }
+    if (id === currentCardId) {
+        yield call(goToBoard, currentBoardId);
+    }
 
-  yield call(updateCard, id, {
-    boardId,
-    listId,
-    position,
-  });
+    yield call(updateCard, id, {
+        boardId,
+        listId,
+        position,
+    });
 }
 
 export function* transferCurrentCard(boardId, listId, index) {
-  const { cardId } = yield select(selectors.selectPath);
+    const { cardId } = yield select(selectors.selectPath);
 
-  yield call(transferCard, cardId, boardId, listId, index);
+    yield call(transferCard, cardId, boardId, listId, index);
 }
 
 export function* deleteCard(id) {
-  const { cardId, boardId } = yield select(selectors.selectPath);
+    const { cardId, boardId } = yield select(selectors.selectPath);
 
-  if (id === cardId) {
-    yield call(goToBoard, boardId);
-  }
+    if (id === cardId) {
+        yield call(goToBoard, boardId);
+    }
 
-  yield put(actions.deleteCard(id));
+    yield put(actions.deleteCard(id));
 
-  let card;
-  try {
-    ({ item: card } = yield call(request, api.deleteCard, id));
-  } catch (error) {
-    yield put(actions.deleteCard.failure(id, error));
-    return;
-  }
+    let card;
+    try {
+        ({ item: card } = yield call(request, api.deleteCard, id));
+    } catch (error) {
+        yield put(actions.deleteCard.failure(id, error));
+        return;
+    }
 
-  yield put(actions.deleteCard.success(card));
+    yield put(actions.deleteCard.success(card));
 }
 
 export function* deleteCurrentCard() {
-  const { cardId } = yield select(selectors.selectPath);
+    const { cardId } = yield select(selectors.selectPath);
 
-  yield call(deleteCard, cardId);
+    yield call(deleteCard, cardId);
 }
 
 export function* handleCardDelete(card) {
-  const { cardId, boardId } = yield select(selectors.selectPath);
+    const { cardId, boardId } = yield select(selectors.selectPath);
 
-  if (card.id === cardId) {
-    yield call(goToBoard, boardId);
-  }
+    if (card.id === cardId) {
+        yield call(goToBoard, boardId);
+    }
 
-  yield put(actions.handleCardDelete(card));
+    yield put(actions.handleCardDelete(card));
 }
 
 export default {
-  createCard,
-  handleCardCreate,
-  updateCard,
-  updateCurrentCard,
-  moveCard,
-  moveCurrentCard,
-  transferCard,
-  transferCurrentCard,
-  handleCardUpdate,
-  deleteCard,
-  deleteCurrentCard,
-  handleCardDelete,
+    createCard,
+    createSingleCard,
+    handleCardCreate,
+    updateCard,
+    updateCurrentCard,
+    moveCard,
+    moveCurrentCard,
+    transferCard,
+    transferCurrentCard,
+    handleCardUpdate,
+    deleteCard,
+    deleteCurrentCard,
+    handleCardDelete,
 };
